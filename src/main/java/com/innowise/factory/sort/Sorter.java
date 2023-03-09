3048ae34257678bd9814e4cac7c4f0fa4be1ee6b
Daria nasennik <nasennik03@gmail.com>
2023-03-09 14:10:57 +0300

    add sorter class

---
+++ src/main/java/com/innowise/factory/sort/Sorter.java
@@ -0,0 +1,19 @@
+package com.innowise.factory.sort;
+
+import com.innowise.factory.basket.Basket;
+import com.innowise.factory.model.ball.Ball;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Sorter<T extends Ball>{
+    Basket<T> basket;
+    public Sorter(Basket<T> basket) {
+        this.basket = basket;
+    }
+
+    public List<T> sort(SortType sortFunction, Comparator<T> comparator) {
+       return sortFunction.getSortable().sort(basket.getBasketContent(), comparator);
+    }
+
+}
